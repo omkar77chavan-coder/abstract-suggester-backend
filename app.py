3736dfdb3372ae0c776bea9b54613afec317c2a1
omkar77chavan-coder <omkar77chavan@gmail.com>
2025-08-17 20:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -368,5 +368,8 @@ def suggest():
         return jsonify({"error": str(e)}), 500
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render gives you PORT env var
+    app.run(host="0.0.0.0", port=port)
